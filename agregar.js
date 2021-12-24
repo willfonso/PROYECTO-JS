dd5a231298ce1501c5a4ff1b73a88d9f5e91103c
William Alfonso <willfonso@gmail.com>
2021-12-24 18:49:07 -0300

    Add bootstraps class

--- agregar.js
+++ agregar.js
@@ -1,28 +1,38 @@
-function agregar(){
-    //console.log("Agregado")
-    function Mercaderia(codigo,descripcion,rubro,costo) {
-        this.codigo=codigo;
-        this.descripcion=descripcion;
-        this.rubro=rubro;
-        this.costo=costo;
+var baseDatos = [];
+
+class Mercaderia {
+    constructor(codigo, descripcion, rubro, costo) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.rubro = rubro;
+        this.costo = costo;
     }
 }
 
-var codigoAgregar = document.getElementById("codigo").value;
-var descripcionAgregar = document.getElementById("descripcion").value;
-var rubroAgregar = document.getElementById("rubro").value;
-var costoAgregar = document.getElementById("costo").value;
 
-nuevaMercaderia = new Mercaderia(codigoAgregar,descripcionAgregar,rubroAgregar,costoAgregar);
-console.log(nuevaMercaderia);
-capturar();
-}
+function agregar() {
+    let codigoAgregar = document.getElementById("codigo").value;
+    var descripcionAgregar = document.getElementById("descripcion").value;
+    var rubroAgregar = document.getElementById("rubro").value;
+    var costoAgregar = document.getElementById("costo").value;
 
-var baseDatos =[];
-function capturar(){
-    baseDatos.push(nuevaMercaderia);
+    nuevaMercaderia = new Mercaderia(
+        codigoAgregar,
+        descripcionAgregar,
+        rubroAgregar,
+        costoAgregar
+    );
+
     console.log(nuevaMercaderia);
-    document.getElementById("tabla").innerHTML += '<tbody><td>' +nuevaMercaderia.codigo+
-    '<tbody><td>' +nuevaMercaderia.descripcion+'<tbody><td>' 
-    +nuevaMercaderia.rubro+'<tbody><td>' +nuevaMercaderia.costo+'</td></rbody>';
+    baseDatos.push(nuevaMercaderia);
+
+    document.getElementById("tbody").innerHTML += `
+        <tr>
+            <th>${nuevaMercaderia.codigo}</th>
+            <th>${nuevaMercaderia.descripcion}</th>
+            <th>${nuevaMercaderia.rubro}</th>
+            <th>${nuevaMercaderia.costo}</th>
+        </tr>
+    `;
+
 }
