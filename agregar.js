9c5c0b56e17f1dfe86b6ed93b0f235ecbace781f
willfonso <96026820+willfonso@users.noreply.github.com>
2022-04-11 22:06:00 -0300

    Add files via upload

--- agregar.js
+++ agregar.js
@@ -1,38 +1,52 @@
 var baseDatos = [];
 
 class Mercaderia {
-    constructor(codigo, descripcion, rubro, costo) {
+    constructor(codigo, descripcion, rubro, costo, rentabilidad, precio) {
         this.codigo = codigo;
         this.descripcion = descripcion;
         this.rubro = rubro;
         this.costo = costo;
+        this.rentabilidad = rentabilidad;
+        this.precio = precio;
     }
 }
 
-
-function agregar() {
+function agregar(e) {
     let codigoAgregar = document.getElementById("codigo").value;
     var descripcionAgregar = document.getElementById("descripcion").value;
     var rubroAgregar = document.getElementById("rubro").value;
     var costoAgregar = document.getElementById("costo").value;
+    var rentabilidadAgregar = document.getElementById("rentabilidad").value;
+    var precioAgregar = document.getElementById("precio").value;
 
     nuevaMercaderia = new Mercaderia(
         codigoAgregar,
         descripcionAgregar,
         rubroAgregar,
-        costoAgregar
+        costoAgregar,
+        rentabilidadAgregar,
+        precioAgregar
     );
 
     console.log(nuevaMercaderia);
     baseDatos.push(nuevaMercaderia);
 
-    document.getElementById("tbody").innerHTML += `
+       document.getElementById("tbody").innerHTML += `
         <tr>
             <th>${nuevaMercaderia.codigo}</th>
             <th>${nuevaMercaderia.descripcion}</th>
             <th>${nuevaMercaderia.rubro}</th>
             <th>${nuevaMercaderia.costo}</th>
+            <th>${nuevaMercaderia.rentabilidad}</th>
+            <th>${nuevaMercaderia.precio}</th>
+            <button name="eliminar" class="btn btn-danger">Eliminar</button>
+
         </tr>
     `;
-
+    document.getElementById('tbody').addEventListener('click', function(e){
+        const nuevaMercaderia = new nuevaMercaderia();
+        nuevaMercaderia.eliminarProducto(e.target);
+        });
 }
+
+
---
+++ funciones.js
@@ -0,0 +1,34 @@
+<<<<<<< HEAD
+class producto {
+constructor (nombre, categoria, precio, stock) {
+    this.nombre = nombre.toUpperCase ();
+    this.categoria = categoria.toUpperCase();
+    this.precio = parseFloat(precio);
+    this.stock = parseInt(stock);
+
+}
+descuento(){
+    if pr
+}
+
+=======
+let seguir = true;  
+while (seguir === true) {
+function calcular () {
+let producto = prompt(`A que producto le desea calcular el costo?`);
+var producto1 =producto.toUpperCase();
+let precio = parseInt(prompt(`Cual es el precio de venta de: ${producto1}`));
+alert(`Ingresaste: ${producto1} \n Tiene un precio de venta ${precio}`);
+let rentabilidad = prompt(`Cual es el procentaje de rentabilidad de: ${producto1}`);
+alert(`La rentabilidad de ${producto1} es ${rentabilidad} %`);
+  let conversion = rentabilidad / 100;
+  console.log(conversion);
+  let costo = precio - (precio * conversion);
+  alert(`El costo de tu ${producto1} es ${costo}`)
+}
+
+  calcular ();
+  seguir = confirm(`Desea calcular otro costo?`);
+ 
+>>>>>>> 8f22f9bbb72e5a298cbc4986543e56fdff008265
+}
